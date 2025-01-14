8cd4d62c010093feaafc9928197e1bb7ab648b3f
AleksandR <79250403617@ya.ru>
2025-01-14 20:41:09 +0300

    fix

---
+++ HW_1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace HW_1;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        if (args.Length == 3)
+        {
+
+            int result = 0;
+            int num1 = int.Parse(args[0]);
+            string operation = args[1];
+            int num2 = int.Parse(args[2]);
+
+            if (operation == "+")
+            {
+                result = num1 + num2;
+            }
+            else if (operation == "-")
+            {
+                result = num1 - num2;
+            }
+            else if (operation == "/")
+            {
+                result = num1 / num2;
+            }
+            else if (operation == "x")
+            {
+                result = num1 * num2;
+            }
+            Console.WriteLine($"Результат: {num1} {operation} {num2} = {result}");
+        }
+        else
+            Console.WriteLine("Введите: число знак(+ - / x) число");
+
+    }
+}
+
---
+++ HW_2/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace HW_2;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[,] Arr = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+
+        Console.WriteLine("Исходный массив\n");
+        PrintArray(Arr);
+
+        int y = Arr.GetLength(0);
+        int x = Arr.GetLength(1);
+        int[] sortArr = new int[x * y];
+
+        int count = 0;
+
+        foreach (var item in Arr)
+        {
+            sortArr[count] = item;
+            count++;
+        }
+
+        count = 0;
+
+        Array.Sort(sortArr);
+
+        for (int n = 0; n < y; n++)
+        {
+            for (int m = 0; m < x; m++)
+            {
+                Arr[n, m] = sortArr[count];
+                count++;
+            }
+        }
+
+        Console.WriteLine("Сортированный массив\n");
+        PrintArray(Arr);
+
+    }
+    static void PrintArray(int[,] tmpArr)
+    {
+        int i = 0;
+
+        foreach (var item in tmpArr)
+        {
+            Console.Write(item + " ");
+            i++;
+            if (i % tmpArr.GetLength(1) == 0)
+            {
+                Console.WriteLine();
+                i = 0;
+            }
+        }
+        Console.WriteLine();
+    }
+}
